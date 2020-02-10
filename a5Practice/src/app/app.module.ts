5972368aed0cb4afec86393a0876e529d9aaca01
Jordan Gaza <Gaza>
2020-02-09 21:56:36 -0500

    She LIIIIVVVEEESSS

--- a5Practice/src/app/app.module.ts
+++ a5Practice/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Practice1Component } from './components/practice1/practice1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Practice1Component
   ],
   imports: [
     BrowserModule,
